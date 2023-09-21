8cdb3e63c6f116145aa842546dd22e2636d288c7
EthanMckeen <101148661+EthanMckeen@users.noreply.github.com>
2023-09-20 17:49:57 -0700

    Player Movement
    Scripts for player movement and a scene for dev testing has been created

---
+++ Platformer_Prototype0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update(){
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    }
+
+    void FixedUpdate() { 
+        //move the player
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    
+    }
+}
